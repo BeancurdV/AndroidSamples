4e5143bb8e64906efcd8f308fb075d331beb5fbd
beancurdv <z69yuan@163.com>
2023-10-17 18:57:50 +0800

    存在函数重载

--- app/src/main/java/com/beancurd/androidsamples/MainActivity.kt
+++ app/src/main/java/com/beancurd/androidsamples/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    var total = "@@@###@@@"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,4 +23,11 @@ class MainActivity : AppCompatActivity() {
     fun xx(x: Int, y: Int) {
         Log.d("XSum", (x + y).toString())
     }
+
+    fun xx(x:String) :String {
+        total += x
+        return x.lowercase()
+    }
+
+    fun secret() = total
 }
